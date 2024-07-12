ef88c1e4afefee3188c82bd16a9632c50f421423
imsamaritan <aicodemate.talk@gmail.com>
2024-07-12 13:30:19 +0200

    Indenting

--- app/index.js
+++ app/index.js
@@ -1,39 +1,39 @@
 import App from "./components/App.js";
 import {
- pattern,
- inputField,
- inputIcon,
- invalidSettings,
- validSettings
+   pattern,
+   inputField,
+   inputIcon,
+   invalidSettings,
+   validSettings
 } from "./components/shared/_Private.js";
 
 //Wire keyup event on the input
 inputField.addEventListener('keyup', validate);
 
 function validState() {
- inputIcon.className = "fa fa-check-circle";
- App.clearSettings(invalidSettings);
- App.addSettings(validSettings);
+   inputIcon.className = "fa fa-check-circle";
+   App.clearSettings(invalidSettings);
+   App.addSettings(validSettings);
 }
 
 function invalidState() {
- inputIcon.className = "fa fa-envelope";
- App.clearSettings(validSettings);
- App.addSettings(invalidSettings);
+   inputIcon.className = "fa fa-envelope";
+   App.clearSettings(validSettings);
+   App.addSettings(invalidSettings);
 }
 
 function validate({ target }) {
- const inputValue = target.value.trim();
- const validateEmail = pattern.test(inputValue);
+   const inputValue = target.value.trim();
+   const validateEmail = pattern.test(inputValue);
 
- if (validateEmail)
-  validState();
- else
-  invalidState();
+   if (validateEmail)
+      validState();
+   else
+      invalidState();
 
- if (inputValue.length === 0) {
-    App.clearSettings(invalidSettings);
- }
- 
+   if (inputValue.length === 0) {
+      App.clearSettings(invalidSettings);
+   }
+
 }
 
